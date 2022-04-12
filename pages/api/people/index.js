a60cabdaa850d973ee36eb8a8ec1a471e2bd0d9d
Brandon Wong <brandonwongck@gmail.com>
2022-04-12 17:25:28 +0800

    Try json settings

--- pages/api/people/index.js
+++ pages/api/people/index.js
@@ -1,6 +1,6 @@
 import { people } from '../../../data'
 
 export default function handler(req, res) {
-  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate=590');
+  // res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate=590');
   res.status(200).json(people)
 }
